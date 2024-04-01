8c2f5b44027404586f3d2d78653081d059362c24
Samuel Oluwade <samueloluwade.dev@gmail.com>
2024-03-31 22:49:07 -0500

    first commit

---
+++ models/Conversation.ts
@@ -0,0 +1,11 @@
+export interface Conversation {
+    id: number;
+    name: string;
+    email: string;
+    role: string;
+    telephone: string;
+    inquiry: string;
+    title: string;
+    message: string;
+    thread_id: string;
+}
